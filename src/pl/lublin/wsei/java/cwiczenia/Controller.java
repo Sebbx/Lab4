6c9cdf8ee43b7c763776388e799fe628335cd2fd
Sebbx <56113951+Sebbx@users.noreply.github.com>
2021-05-10 15:44:12 +0200

    Lab 4b| 5. dodanie kodu umożliwiającego zmiane adresu strony oraz miniatury

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,29 +1,54 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import  javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class Controller {
     public Label lbFile;
+    public ImageView imgMiniatura;
+    public TextField txtAdresStrony;
+    public Button btnPrzejdzDoStrony;
+    public Button btnPokazInfografike;
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
 
     public ListView lstInfografiki;
     ObservableList<String> tytuly = FXCollections.observableArrayList();
     GusInfoGraphicList igList;
-
     @FXML
     public void initialize()
     {
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val) {
+                int index = new_val.intValue();
+                if (index != -1){
+                    txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);
+                    Image image = new Image(igList.infografiki.get(index).adresMiniaturki);
+                    imgMiniatura.setImage(image);
+                }
+                else {
+                    txtAdresStrony.setText("");
+                    imgMiniatura.setImage(null);
+                }
+            }
+        });
     }
 
     public void btnOpenFileAction(ActionEvent actionEvent){
